83a8f5273f48cca0f37e1e4a3667053eddbf9e28
Jon Pelc <thisisjonpelc@gmail.com>
2018-09-14 23:01:03 -0500

    Added Addux model

---
+++ server/models/addux.js
@@ -0,0 +1,86 @@
+const mongoose = require("mongoose");
+
+const Addux = mongoose.model("Addux", {
+    name:{
+        type:String,
+        required:true,
+        minlength:1,
+        trim:true
+    },
+    objective:{
+        input:{
+            type:String,
+            trim:true
+        },
+        comments:{
+            type: Schema.Types.ObjectId,
+            ref: "Comment",
+        }
+    },
+    goals:{
+        inputs:[
+            {
+                type:String,
+                trim:true
+            }
+        ],
+        comments: {
+            type:Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    },
+    projects:{
+        inputs:[
+            {
+                type:String,
+                trim:true
+            }
+        ],
+        comments: {
+            type:Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    },
+    timelines:{
+        inputs:[
+            {
+                type:String,
+                trim:true
+            }
+        ],
+        comments: {
+            type:Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    },
+    projectOwner:{
+        inputs:[
+            {
+                type:String,
+                trim:true
+            }
+        ],
+        comments: {
+            type:Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    },
+    expertise:{
+        inputs:[
+            {
+                type:String,
+                trim:true
+            }
+        ],
+        comments: {
+            type:Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    },
+    _creator: {
+        type:mongoose.Schema.Types.ObjectId,
+        required:true
+    }
+});
+
+module.exports = {Addux};

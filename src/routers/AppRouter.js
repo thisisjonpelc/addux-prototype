57ff716a4af9893a3dac527ded3e0b55e74134b2
Jon Pelc <thisisjonpelc@gmail.com>
2018-11-10 22:32:58 -0600

    Cleaned up AppRouter.js

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,26 +1,11 @@
 import React from "react";
-import {connect} from 'react-redux';
 import {Router, Route, Switch, Redirect} from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 import {StripeProvider} from 'react-stripe-elements';
-import axios from 'axios';
 
-import AdduxApp from "../components/AdduxApp";
-import AdduxWrapper from "../components/AdduxWrapper";
-import LoginPage from "../components/LoginPage";
-import SubscribePage from './../components/SubscribePage';
-import ResetRequestPage from './../components/ResetRequestPage';
-import ResetPasswordPage from './../components/ResetPasswordPage';
 import ShareAddux from './../components/ShareAddux';
-import LoadingPage from './../components/LoadingPage';
-import TestPage from './../components/TestPage';
 
 import ProtectedRouter from './ProtectedRouter';
-
-import {login} from './../actions/auth';
-
-import PublicRoute from "./PublicRoute";
-import PrivateRoute from './PrivateRoute';
 
 export const history = createHistory();
 

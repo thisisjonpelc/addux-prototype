b70ecb41bd02766f89c9f2d43881ad9ea81fbdf1
Jon Pelc <thisisjonpelc@gmail.com>
2018-12-05 21:30:20 -0600

    Removed timer from SalesHeader

--- src/components/SalesHeader.js
+++ src/components/SalesHeader.js
@@ -34,43 +34,47 @@ class SalesHeader extends React.Component{
         //console.log(this.state.timeDifference.asSeconds());
         //console.log(this.state.timeDifference.days());
 
+        //Timer to add back later
+
+
+        // <div className='timer'>
+        //             <div className='timer__block'>
+        //                 <p className='timer__number'>
+        //                     {`${this.state.timeDifference.days() < 10 ? '0' : ''}${this.state.timeDifference.days()}`}
+        //                 </p>
+        //                 <p className='timer__label'>
+        //                     Days
+        //                 </p>
+        //             </div>
+        //             <div className='timer__block'>
+        //                 <p className='timer__number'>
+        //                     {`${this.state.timeDifference.hours() < 10 ? '0' : ''}${this.state.timeDifference.hours()}`}
+        //                 </p>
+        //                 <p className='timer__label'>
+        //                     Hours
+        //                 </p>
+        //             </div>
+        //             <div className='timer__block'>
+        //                 <p className='timer__number'>
+        //                     {`${this.state.timeDifference.minutes() < 10 ? '0':''}${this.state.timeDifference.minutes()}`}
+        //                 </p>
+        //                 <p className='timer__label'>
+        //                     Minutes
+        //                 </p>
+        //             </div>
+        //             <div className='timer__block'>
+        //                 <p className='timer__number'>
+        //                     {`${this.state.timeDifference.seconds() < 10 ? '0':''}${this.state.timeDifference.seconds()}`}
+        //                 </p>
+        //                 <p className='timer__label'>
+        //                     Seconds
+        //                 </p>
+        //             </div>
+        //          </div>
+
         return (
             <header className='sales-header'>
                 <img src='/img/addux-logo.png' className='sales-header__logo' />
-                <div className='timer'>
-                    <div className='timer__block'>
-                        <p className='timer__number'>
-                            {`${this.state.timeDifference.days() < 10 ? '0' : ''}${this.state.timeDifference.days()}`}
-                        </p>
-                        <p className='timer__label'>
-                            Days
-                        </p>
-                    </div>
-                    <div className='timer__block'>
-                        <p className='timer__number'>
-                            {`${this.state.timeDifference.hours() < 10 ? '0' : ''}${this.state.timeDifference.hours()}`}
-                        </p>
-                        <p className='timer__label'>
-                            Hours
-                        </p>
-                    </div>
-                    <div className='timer__block'>
-                        <p className='timer__number'>
-                            {`${this.state.timeDifference.minutes() < 10 ? '0':''}${this.state.timeDifference.minutes()}`}
-                        </p>
-                        <p className='timer__label'>
-                            Minutes
-                        </p>
-                    </div>
-                    <div className='timer__block'>
-                        <p className='timer__number'>
-                            {`${this.state.timeDifference.seconds() < 10 ? '0':''}${this.state.timeDifference.seconds()}`}
-                        </p>
-                        <p className='timer__label'>
-                            Seconds
-                        </p>
-                    </div>
-                </div>
             </header>
         );
     }
